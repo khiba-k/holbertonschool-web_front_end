fba7d8ca8820455f0d41a169c7dc343c6bc4d16b
khiba-k <khibakoenane@gmail.com>
2025-01-21 15:38:03 +0200

    task12 complete

---
+++ Javascript_advanced/12-room_area.js
@@ -0,0 +1,9 @@
+const roomDimensions = {
+    width: 50,
+    height: 100,
+    getArea: function() {
+        return this.width * this.height;
+    }
+}
+
+const boundGetArea = roomDimensions.getArea.bind(roomDimensions);
