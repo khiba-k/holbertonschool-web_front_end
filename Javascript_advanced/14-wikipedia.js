b21a1ed333364689921398d64cf81616e71f953f
khiba-k <khibakoenane@gmail.com>
2025-01-21 16:12:28 +0200

    task14 in progress

---
+++ Javascript_advanced/14-wikipedia.js
@@ -0,0 +1,10 @@
+function createElement(data) {
+  const paragraph = document.createElement("p");
+  paragraph.textContent = data;
+
+  document.body.appendChild(paragraph);
+}
+
+function queryWikipedia(callback) {
+    
+}
