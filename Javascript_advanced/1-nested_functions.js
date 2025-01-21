037b2dbfe1b01f32e9028bf954bccea65fc3d458
khiba-k <khibakoenane@gmail.com>
2025-01-21 10:40:05 +0200

    task1 & 2

---
+++ Javascript_advanced/1-nested_functions.js
@@ -0,0 +1,23 @@
+const globalVariable = "Welcome";
+
+function outer() {
+  const course = "Holberton";
+
+  alert(globalVariable);
+
+  function inner() {
+    const exclamation = "!";
+
+    alert(globalVariable + " " + course);
+
+    function inception() {
+      alert(globalVariable + " " + course + " " + exclamation);
+    }
+
+    inception();
+  }
+
+  inner();
+}
+
+outer();
