8aebb91c82b99d42f30bb6ea891cff5396fc9418
khiba-k <khibakoenane@gmail.com>
2025-01-22 12:51:48 +0200

    task 14 complete

--- Javascript_advanced/14-wikipedia.js
+++ Javascript_advanced/14-wikipedia.js
@@ -6,5 +6,20 @@ function createElement(data) {
 }
 
 function queryWikipedia(callback) {
-    
+  const xhttpReq = new XMLHttpRequest();
+  xhttpReq.open(
+    "GET",
+    "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow&origin=*",
+    true
+  );
+
+  xhttpReq.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      callback(xhttpReq.responseText);
+    }
+  };
+
+  xhttpReq.send();
 }
+
+queryWikipedia(createElement);
