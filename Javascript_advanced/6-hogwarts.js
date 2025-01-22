c81f69af4648c8350f41bff20c97f9afe2a73d23
khiba-k <khibakoenane@gmail.com>
2025-01-22 13:23:02 +0200

    task 6 complete

--- Javascript_advanced/6-hogwarts.js
+++ Javascript_advanced/6-hogwarts.js
@@ -1,5 +1,47 @@
-class hogwarts {
-    constructor(privateScore = 0, name = null) {
-        
-    }
+class studentHogwarts {
+  #privateScore = 0;
+  #name = null;
+
+  #changeScoreBy(points) {
+    this.#privateScore += points;
+  }
+
+  setName(newName) {
+    this.#name = newName;
+    return this;
+  }
+
+  rewardStudent() {
+    this.#changeScoreBy(1);
+    return this;
+  }
+
+  penalizeStudent() {
+    this.#changeScoreBy(-1);
+    return this;
+  }
+
+  getScore() {
+    return `${this.#name}: ${this.#privateScore}`;
+  }
 }
+
+const harry = new studentHogwarts();
+harry
+  .setName("Harry")
+  .rewardStudent()
+  .rewardStudent()
+  .rewardStudent()
+  .rewardStudent();
+
+console.log(harry.getScore());
+
+const draco = new studentHogwarts();
+draco
+  .setName("Draco")
+  .rewardStudent()
+  .penalizeStudent()
+  .penalizeStudent()
+  .penalizeStudent();
+
+console.log(draco.getScore());
